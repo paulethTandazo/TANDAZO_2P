c20a7d21cedcf2c29d54db9ea45c96d298c85b60
paulethTandazo <ptandazo@espol.edu.ec>
2023-01-15 21:36:43 -0500

    "creación de la clase ValorInsuficienteException"

---
+++ src/main/java/Excecpcion/ValorInsuficienteException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excecpcion;
+
+/**
+ *
+ * @author marle
+ */
+public class ValorInsuficienteException extends Exception{
+    public ValorInsuficienteException(String message){
+        super(message);
+    }
+}
