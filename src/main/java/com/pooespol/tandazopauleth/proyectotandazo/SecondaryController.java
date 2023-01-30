1731c5bf5cc91e4e907d35a78b9778c60c7b1ce6
paulethTandazo <ptandazo@espol.edu.ec>
2023-01-30 15:14:21 -0500

    "comentamos los metodos  para que se genere el javadoc e implementacion de metodos para cargar a escena cuando selecione realizar pedido o encontrar lugar en el mapa"

--- src/main/java/com/pooespol/tandazopauleth/proyectotandazo/SecondaryController.java
+++ src/main/java/com/pooespol/tandazopauleth/proyectotandazo/SecondaryController.java
@@ -14,6 +14,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+/**
+ * Clase SecondaryController
+ *
+ * @author marle
+ */
 public class SecondaryController implements Initializable {
 
     @FXML
@@ -23,18 +28,33 @@ public class SecondaryController implements Initializable {
     @FXML
     private Button btnLocal;
 
+    /**
+     * Método initialize se invoca automáticamente cuando la vista está cargada
+     * y se utiliza para inicializar los componentes y asignarles los valores o
+     * eventos necesarios antes de que la vista sea mostrada al usuario
+     *
+     * @param url recibe un URl como parametro
+     * @param rb recibe un ResourceBundle como parametro
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         lblNombreUsuario.setText("Bienvenid@" + "  " + App.usuario.getNombre());
 //        lblNombreUsuario.setTextFill(Color.WHITE);
     }
 
+    /**
+     * Método que se encargara de cambiar de vista cuando se presione en el
+     * boton encontrarLocal
+     *
+     * @param event1 recibe como parametro un evento
+     * @throws IOException lanza una excepcion verificada
+     */
     @FXML
     public void encontrarLocal(ActionEvent event1) throws IOException {
         Stage s = new Stage();
         FXMLLoader fxmlloader = new FXMLLoader(App.class.getResource("Local.fxml"));
         Parent root = fxmlloader.load();
-        Scene scene = new Scene(root, 640, 400);
+        Scene scene = new Scene(root, 1200, 600);
         s.setScene(scene);
         s.setTitle("Local");
         s.show();
@@ -43,17 +63,28 @@ public class SecondaryController implements Initializable {
         stage.close();
     }
 
+    /**
+     * Metodo que se encarga de cambiar de vista cuando se presione el boton
+     * realizarPedido
+     *
+     * @param event2 recibe como parametro un ActionEvent
+     */
     @FXML
-    public void realizarPedido(ActionEvent event2) throws IOException {
-        Node source = (Node) event2.getSource();     //Me devuelve el elemento al que hice click
-        Stage stage = (Stage) source.getScene().getWindow();    //Me devuelve la ventana donde se encuentra el elemento
-        stage.close();
-        Stage s = new Stage();
-        FXMLLoader fxmlloader = new FXMLLoader(App.class.getResource("Pedido.fxml"));
-        Parent root = fxmlloader.load();
-        Scene scene = new Scene(root, 800, 500);
-        s.setScene(scene);
-        s.setTitle("Pedido");
-        s.show();
+    public void realizarPedido(ActionEvent event2) {
+        try {
+            Node source = (Node) event2.getSource();     //Me devuelve el elemento al que hice click
+            Stage stage = (Stage) source.getScene().getWindow();    //Me devuelve la ventana donde se encuentra el elemento
+            stage.close();
+            Stage s = new Stage();
+            FXMLLoader fxmlloader = new FXMLLoader(App.class.getResource("Pedido.fxml"));
+            Parent root = fxmlloader.load();
+            Scene scene = new Scene(root, 800, 500);
+            s.setScene(scene);
+            s.setResizable(false);
+            s.setTitle("Pedido");
+            s.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
