5966f49ea5315fb4929c549753f52109120989af
paulethTandazo <ptandazo@espol.edu.ec>
2023-01-30 15:07:11 -0500

    "comentarios de la clase Menu para generar javadoc"

--- src/main/java/Menu/Menu.java
+++ src/main/java/Menu/Menu.java
@@ -5,44 +5,70 @@
 package Menu;
 
 /**
- *
+ * Clase que contiene los atributos del MENU
  * @author marle
  */
 public class Menu {
     private String descripcion;
     private double precio;
     private tipoMenu tipo;
-
+/**
+ * constructor de la clase menu
+ * @param descripcion parametro de descripcion
+ * @param precio parametro del precio
+ * @param tipo parametro del tipo
+ */
     public Menu(String descripcion, double precio, tipoMenu tipo) {
         this.descripcion = descripcion;
         this.precio = precio;
         this.tipo = tipo;
     }
-
+/**
+ * meotod que retorna la descrpcioon
+ * @return retorna la descripcion
+ */
     public String getDescripcion() {
         return descripcion;
     }
-
+/**
+ * metodo que actualiza la descrpcion
+ * @param descripcion parametro de la descripcion
+ */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
+/**
+ * metodo que retorna el precio
+ * @return reotrna el precio
+ */
     public double getPrecio() {
         return precio;
     }
-
+/**
+ * metodo que actualiza el precio
+ * @param precio parametro del precio
+ */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+/**
+ * metodo que retorna el tipo
+ * @return retorna el tipo
+ */
     public tipoMenu getTipo() {
         return tipo;
     }
-
+/**
+ * metodo que actualiza el tipo
+ * @param tipo parametro del tipo
+ */
     public void setTipo(tipoMenu tipo) {
         this.tipo = tipo;
     }
-
+/**
+ * metodo toString de la clase MENU
+ * @return retorna el formato con los atributos
+ */
     @Override
     public String toString() {
         return "Menu{" + "descripcion=" + descripcion + ", precio=" + precio + ", tipo=" + tipo + '}';
